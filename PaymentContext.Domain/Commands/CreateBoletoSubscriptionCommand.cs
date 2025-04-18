4f7bafe83b2ccdf7515bdc60355b71f4fe673312
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2025-04-17 21:34:39 -0300

    Add Notifications (commands)

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable<Notification>, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -30,5 +29,45 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract<CreateBoletoSubscriptionCommand>()
+                .Requires()
+                .IsNotNullOrEmpty(FirstName, "FirstName", "Nome é obrigatório")
+                .IsNotNullOrEmpty(LastName, "LastName", "Sobrenome é obrigatório")
+
+                // Documento e Email
+                .IsNotNullOrEmpty(Document, "Document", "Documento é obrigatório")
+                .IsNotNullOrEmpty(Email, "Email", "Email é obrigatório")
+                .IsEmail(Email, "Email", "Email inválido")
+
+                // Boleto
+                .IsNotNullOrEmpty(BarCode, "BarCode", "Código de barras é obrigatório")
+                .IsNotNullOrEmpty(BoletoNumber, "BoletoNumber", "Número do boleto é obrigatório")
+
+                // Pagamento
+                .IsNotNullOrEmpty(PaymentNumber, "PaymentNumber", "Número do pagamento é obrigatório")
+                .IsGreaterThan(Total, 0, "Total", "Total deve ser maior que zero")
+                .IsGreaterThan(TotalPaid, 0, "TotalPaid", "Total pago deve ser maior que zero")
+                .IsLowerOrEqualsThan(PaidDate, DateTime.Now, "PaidDate", "A data de pagamento não pode ser futura")
+                .IsGreaterThan(ExpireDate, DateTime.Now, "ExpireDate", "A data de expiração deve ser futura")
+
+                // Pagador
+                .IsNotNullOrEmpty(Payer, "Payer", "Pagador é obrigatório")
+                .IsNotNullOrEmpty(PayerDocument, "PayerDocument", "Documento do pagador é obrigatório")
+                .IsNotNullOrEmpty(PayerEmail, "PayerEmail", "Email do pagador é obrigatório")
+                .IsEmail(PayerEmail, "PayerEmail", "Email do pagador inválido")
+
+                // Endereço
+                .IsNotNullOrEmpty(Street, "Street", "Rua é obrigatória")
+                .IsNotNullOrEmpty(Number, "Number", "Número é obrigatório")
+                .IsNotNullOrEmpty(Neighborhood, "Neighborhood", "Bairro é obrigatório")
+                .IsNotNullOrEmpty(City, "City", "Cidade é obrigatória")
+                .IsNotNullOrEmpty(State, "State", "Estado é obrigatório")
+                .IsNotNullOrEmpty(Country, "Country", "País é obrigatório")
+                .IsNotNullOrEmpty(ZipCode, "ZipCode", "CEP é obrigatório")
+            );
+        }
     }
 }
--- PaymentContext.Shared/Commands/ICommand.cs
+++ PaymentContext.Shared/Commands/ICommand.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace PaymentContext.Shared.Commands
 {
     public interface ICommand
     {
-        
+        void Validate();
     }
 }
