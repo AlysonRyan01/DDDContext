91f7b409102f9c8cbc6e44255e00347a6ad4d6ca
Alyson Ryan Ullirsch <sdhuahdsaih@gmail.com>
2025-04-17 22:03:01 -0300

    create boleto test

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,12 @@
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void test1()
+        {
+            
+        }
+    }
+}
