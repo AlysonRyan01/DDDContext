7a041c0698506449ed8f4b001c32b4a4036d744c
AlysonRyan01 <sdhuahdsaih@gmail.com>
2025-04-19 00:51:47 -0300

    Add studentQueriesTests

---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,12 @@
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
